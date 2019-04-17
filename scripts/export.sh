59af142c29c4848ddaab175c75f21f80c7edec64
matheus maciel <matheusmacielmelchiades@hotmail.com>
2019-04-17 18:39:19 -0300

    Update export.sh

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,11 +1,10 @@
-# read -s -p "USER: " user
-# printf "\n"
-# read -s -p "PASSWORD: " pass
-# printf "\n"
+read -s -p "USER: " user
+printf "\n"
+read -s -p "PASSWORD: " pass
+printf "\n"
 
-# read -p "COLLECTION: " collection
-# printf "\n"
+read -p "COLLECTION: " collection
+printf "\n"
 
 
-# mongoexport -h ds155815.mlab.com:55815 -d carsdb -c $collection -u $user -p $pass -o $1'.json' $@
-# mongoexport -h ds155815.mlab.com:55815 -d carsdb -c api_brands -u admin -p qwer1234 -o $@ 
+mongoexport -h ds155815.mlab.com:55815 -d carsdb -c $collection -u $user -p $pass -o "DATA_BASE/$collection.json" $@
